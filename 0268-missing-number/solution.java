b7f9597009a4f92019467f1428b58f447a23b94c
Gxneshex <sriganeshkv1@gmail.com>
2026-07-20 15:31:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.9 MB (97.87%)

---
+++ 0268-missing-number/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sumN=(n*(n+1))/2,sum=0;
+        for(int i: nums) sum+=i;
+        return sumN-sum;
+    }
+}
