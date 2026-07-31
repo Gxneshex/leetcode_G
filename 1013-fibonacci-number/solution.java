2e41604efa413c3d873f0d30f026316891dcfb02
Gxneshex <sriganeshkv1@gmail.com>
2026-07-31 14:51:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42 MB (53.05%)

---
+++ 1013-fibonacci-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0||n==1)return n;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,dp);
+    }
+    int memo(int n,int dp[]){
+        if(n==0||n==1)return n;
+        if(dp[n]!=-1)return dp[n];
+        dp[n]=memo(n-1,dp)+memo(n-2,dp);
+        return dp[n];
+    }
+}
